f9429a1b8d9e28c8fec339aa565ee7144d12753a
charuzain <charuabhizain@gmail.com>
2023-03-02 21:43:25 -0500

    Reverse a string

---
+++ freecodeCampAlgo/reverseString.js
@@ -0,0 +1,13 @@
+// Reverse the provided string and return the reversed string.
+
+// For example, "hello" should become "olleh".
+
+const reverseString = function(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0;  i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+};
+
+console.log(reverseString("hello"));
